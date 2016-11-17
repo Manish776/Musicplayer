5e817622e17527bd1547b1f99499b012124157a4
Akash Vishnu Sangle <akashsangle377@gmail.com>
2016-11-17 10:24:35 +0530

    Initial commit

---
+++ app/src/main/java/com/mymindakash/musicplayer/Ak.java
@@ -0,0 +1,18 @@
+package com.mymindakash.musicplayer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Akash117759 on 14-11-2016.
+ */
+
+public class Ak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.music_player_layout);
+    }
+}
